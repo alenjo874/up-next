26492d6bfacefc78464da8046dc3c8453756d57c
Alen Jo <alenjo874@gmail.com>
2022-04-19 18:59:29 -0400

    setting up redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import "../src/style/style.css";
+import Main from "./components/main/Main";
+import Party from "./components/party/Party";
+import { useSelector, useDispatch } from "react-redux";
+import { increment } from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counterReducer);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-    <p>Test</p>
+      <h1> {counter}</h1>
+
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button>-</button>
+      <Main />
+      <Party />
     </div>
   );
 }
---
+++ client/src/components/party/Party.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Party() {
+  return (
+    <div className='party-page'>
+        <div className='party-container'>
+      
+        </div>
+    </div>
+  )
+}
+
+export default Party
